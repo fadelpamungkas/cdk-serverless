9df85b0255777e159a60da88f6ec9ad0a7ade4b6
fadelpamungkas <fadelpamungkas3@gmail.com>
2022-04-22 21:11:03 +0700

    initialize

--- cdk_serverless/cdk_serverless_stack.py
+++ cdk_serverless/cdk_serverless_stack.py
@@ -4,12 +4,15 @@ from aws_cdk import (
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from . import serverless
+
 
 class CdkServerlessStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        serverless.WidgetService(self, "Widgets")
         # The code that defines your stack goes here
 
         # example resource
